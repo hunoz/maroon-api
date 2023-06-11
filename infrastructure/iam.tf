e7ae3cf8dae07185dcc7379d63963bcf3a6be806
David Gregory <42326109+hunoz@users.noreply.github.com>
2023-06-10 21:35:36 -0500

    This is required for assume role to work properly

--- infrastructure/iam.tf
+++ infrastructure/iam.tf
@@ -14,3 +14,23 @@ resource "aws_secretsmanager_secret_version" "marron_api_user_key_secret_version
   secret_id     = aws_secretsmanager_secret.maroon_api_user_key_secret.id
   secret_string = jsonencode({ "AccessKeyId" : aws_iam_access_key.maroon_api_user_key.id, "SecretAccessKey" : aws_iam_access_key.maroon_api_user_key.secret })
 }
+
+resource "aws_iam_policy" "maroon_api_user_sts_policy" {
+  name        = "AllowAssumeRole"
+  description = "Allows the user to assume any role anywhere"
+  policy      = jsonencode({
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Action": "sts:AssumeRole",
+            "Resource": "*"
+        }
+    ]
+  })
+}
+
+resource "aws_iam_user_policy_attachment" "maroon_api_user_sts_policy_attachment" {
+  user       = aws_iam_user.maroon_api_user.name
+  policy_arn = aws_iam_policy.maroon_api_user_sts_policy.arn
+}
